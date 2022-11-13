5b0cad1a744f5578a8a0860665be0b9e4b995937
SerhyRymar <serhyrymar@gmail.com>
2022-11-13 14:10:57 +0200

    Done_task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,30 @@
 
+const loginRef = document.querySelector(".login-form");
+
+console.log(loginRef);
+
+loginRef.addEventListener('submit', loginForm)
+
+function loginForm(event) {
+    event.preventDefault();
+
+    const forms = event.currentTarget.elements;
+    const email = forms.email.value;
+    const password = forms.password.value;
+
+    if (email.length === 0 || password.length === 0) {
+        alert('Внесіть дані');
+        return;
+    }
+
+    const formData = {
+        email, password,
+    };
+
+    console.log(formData);
+
+    loginRef.reset();
+
+}
+
+
