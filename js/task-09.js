df70d876cde2e1ba8b64653169b361e2b1ae9084
SerhyRymar <serhyrymar@gmail.com>
2022-11-13 15:11:58 +0200

    Done_task09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const body = document.body;
+// console.log(body);
+const button = document.querySelector(".change-color");
+// console.log(button);
+const colorName = document.querySelector('.color')
+// console.log(colorName);
+
+button.addEventListener("click", () => {
+  body.style.backgroundColor = getRandomHexColor();
+	colorName.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// console.log(getRandomHexColor);
